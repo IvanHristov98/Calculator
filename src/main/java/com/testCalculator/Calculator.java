26f926efae0b13e7ac5a6a1819535fc799606f92
Hristov <ivan.hristov@sap.com>
2019-07-03 17:35:56 +0300

    Totally change calculator

--- src/main/java/com/testCalculator/Calculator.java
+++ src/main/java/com/testCalculator/Calculator.java
@@ -1,39 +1,12 @@
 package com.testCalculator;
 
-class Calculator
+public class Calculator<T>
 {	
 	private Calculator()
 	{}
 	
-	/**
-	 * 
-	 * @param <T>
-	 * @param left
-	 * @param right
-	 * @param operation
-	 * @return
-	 * @throws Exception
-	 */
-	@SuppressWarnings("unchecked")
-	public static<T extends Number> T calculate(T left, T right, String operation) throws Exception
-	{				
-		if (operation.equals("+"))
-		{
-			return (T)(Double.valueOf(left.doubleValue() + right.doubleValue()));
-		}
-		else if (operation.equals("-"))
-		{
-			return (T)Double.valueOf(left.doubleValue() - right.doubleValue());
-		}
-		else if (operation.equals("*"))
-		{
-			return (T)Double.valueOf(left.doubleValue() * right.doubleValue());
-		}
-		else if (operation.equals("/"))
-		{
-			return (T)Double.valueOf(left.doubleValue() / right.doubleValue());
-		}
+	private Calculator(String expression)
+	{
 		
-		throw new Exception("Not a valid sign");
 	}
 }
