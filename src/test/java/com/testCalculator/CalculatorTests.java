78f6841b0c47d9b03bbc036d0c05e49df1a334e6
Hristov <ivan.hristov@sap.com>
2019-07-03 17:43:53 +0300

    Add CalculatorTests

---
+++ src/test/java/com/testCalculator/CalculatorTests.java
@@ -0,0 +1,13 @@
+package com.testCalculator;
+
+import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+
+public class CalculatorTests 
+{
+	@Test
+	public void Sum_PositiveNumbers_Calculated()
+	{
+		
+	}
+}
