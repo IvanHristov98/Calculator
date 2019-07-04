889d9348995c5302f8bd80e5426559655e0dab4e
Hristov <ivan.hristov@sap.com>
2019-07-04 16:58:09 +0300

    Refine calculator tests

--- src/test/java/com/testCalculator/CalculatorTests.java
+++ src/test/java/com/testCalculator/CalculatorTests.java
@@ -8,51 +8,23 @@ public class CalculatorTests
 	private static final double DELTA = 0.01;
 	
 	@Test
-	public void SumAndSubtractNumbers_Calculated() throws Exception
+	public void SumAndSubtractMultipleNumbers_Calculated() throws Exception
 	{
-		Calculator calc = Calculator.constructFromExpression("3 + 3 + 3");
-		assertEquals(9, calc.calculate().doubleValue(), CalculatorTests.DELTA);
-		
-		calc.changeExpression("-3 + 3");
-		assertEquals(0, calc.calculate().doubleValue(), CalculatorTests.DELTA);
-		
-		calc.changeExpression("0 + 0");
-		assertEquals(0, calc.calculate().doubleValue(), CalculatorTests.DELTA);
-		
-		calc  = Calculator.constructFromExpression("1 - 1");
-		assertEquals(0, calc.calculate().doubleValue(), CalculatorTests.DELTA);
-		
-		calc  = Calculator.constructFromExpression("1 - 15");
-		assertEquals(-14, calc.calculate().doubleValue(), CalculatorTests.DELTA);
+		Calculator calc = Calculator.constructFromExpression("3 + 3 + 3 - 3");
+		assertEquals(6, calc.calculate().doubleValue(), CalculatorTests.DELTA);
 	}
 	
 	@Test
-	public void MultiplyNumbers_Calculated() throws Exception
+	public void MultiplyNegativeNumbers_Calculated() throws Exception
 	{
-		Calculator calc  = Calculator.constructFromExpression("0 * 1");
-		assertEquals(0, calc.calculate().doubleValue(), 0);
-		
-		calc  = Calculator.constructFromExpression("1 * 1");
-		assertEquals(1, calc.calculate().doubleValue(), 0);
-		
-		calc  = Calculator.constructFromExpression("-1 * 1");
-		assertEquals(-1, calc.calculate().doubleValue(), 0);
-		
-		calc  = Calculator.constructFromExpression("-1 * -1");
+		Calculator calc  = Calculator.constructFromExpression("-1 * -1");
 		assertEquals(1, calc.calculate().doubleValue(), 0);
 	}
-	
-	/**
-	 * TODO Add divide by zero
-	 * @throws Exception
-	 */
+
 	@Test
-	public void DivideNumbers_Calculated() throws Exception
+	public void DivideByZero_Calculated() throws Exception
 	{
 		Calculator calc  = Calculator.constructFromExpression("0 / 1");
 		assertEquals(0, calc.calculate().doubleValue(), CalculatorTests.DELTA);
-		
-		calc  = Calculator.constructFromExpression("2 / 3");
-		assertEquals(0.66, calc.calculate().doubleValue(), CalculatorTests.DELTA);
 	}
 }
