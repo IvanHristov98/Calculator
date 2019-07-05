ed622e82478af84d2b3bb637ac029d29666884fe
Hristov <ivan.hristov@sap.com>
2019-07-05 14:39:33 +0300

    Change brackets exception to now extends operator misplacement exception

--- src/main/java/com/calculator/exception/BracketsException.java
+++ src/main/java/com/calculator/exception/BracketsException.java
@@ -1,6 +1,6 @@
 package com.calculator.exception;
 
-public final class BracketsException extends OperatorException
+public final class BracketsException extends OperatorMisplacementException
 {
 	private static final long serialVersionUID = -212012675847552595L;
 	
