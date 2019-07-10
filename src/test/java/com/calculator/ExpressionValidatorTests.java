ae355d59f8a7e4ee36b57c9ee21d690e86f3a897
Hristov <ivan.hristov@sap.com>
2019-07-10 15:53:21 +0300

    Change method names.

--- src/test/java/com/calculator/ExpressionValidatorTests.java
+++ src/test/java/com/calculator/ExpressionValidatorTests.java
@@ -12,115 +12,115 @@ public class ExpressionValidatorTests
     }
 
     @Test(expected = NumberMisplacementException.class)
-    public void test_consecutive_getExpression() throws Exception
+    public void test_consecutive_validateExpression() throws Exception
     {
         this.validateExpression("2+4.55.6");
     }
 
     @Test(expected = EmptyExpressionException.class)
-    public void test_emptyExpression_getExpression() throws Exception
+    public void test_emptyExpression_validateExpression() throws Exception
     {
         this.validateExpression("");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_manyLegitimateOperatorsAtBeginning_getExpression() throws Exception
+    public void test_manyLegitimateOperatorsAtBeginning_validateExpression() throws Exception
     {
         this.validateExpression("++1+2");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_consecutiveOperators_getExpression() throws Exception
+    public void test_consecutiveOperators_validateExpression() throws Exception
     {
         this.validateExpression("2+*3");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_invalidTokenException_getExpression() throws Exception
+    public void test_invalidTokenException_validateExpression() throws Exception
     {
         this.validateExpression("1A2");
     }
 
     @Test(expected = BracketsException.class)
-    public void test_numberGluedToTheLeftOfABracketedExpression_getExpression() throws Exception
+    public void test_numberGluedToTheLeftOfABracketedExpression_validateExpression() throws Exception
     {
         this.validateExpression("2(3+4)");
     }
 
     @Test(expected = BracketsException.class)
-    public void test_numberGluedToTheRightOfABracketedExpression_getExpression() throws Exception
+    public void test_numberGluedToTheRightOfABracketedExpression_validateExpression() throws Exception
     {
         this.validateExpression("(3+4)5");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_misplacedOperatorAtStart_getExpression() throws Exception
+    public void test_misplacedOperatorAtStart_validateExpression() throws Exception
     {
         this.validateExpression("*1+2");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_manyMisplacedOperatorsAtBeginning_getExpression() throws Exception
+    public void test_manyMisplacedOperatorsAtBeginning_validateExpression() throws Exception
     {
         this.validateExpression("**/1/2");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_misplacedOperatorAtEnd_getExpression() throws Exception
+    public void test_misplacedOperatorAtEnd_validateExpression() throws Exception
     {
         this.validateExpression("1/2*");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_manyMisplacedOperatorsAtEnd_getExpression() throws Exception
+    public void test_manyMisplacedOperatorsAtEnd_validateExpression() throws Exception
     {
         this.validateExpression("1/2*/*/*");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_singleOperator_getExpression() throws Exception
+    public void test_singleOperator_validateExpression() throws Exception
     {
         this.validateExpression("+");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_misplacedOperatorsStraightAfterOpeningBracket_getExpression() throws Exception
+    public void test_misplacedOperatorsStraightAfterOpeningBracket_validateExpression() throws Exception
     {
         this.validateExpression("(*3)");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_misplacedOperatorsAtEndsWithinBrackets_getExpression() throws Exception
+    public void test_misplacedOperatorsAtEndsWithinBrackets_validateExpression() throws Exception
     {
         this.validateExpression("(4+5*)");
     }
 
     @Test(expected = BracketsException.class)
-    public void test_noClosingBracket_getExpression() throws Exception
+    public void test_noClosingBracket_validateExpression() throws Exception
     {
         this.validateExpression("(1+2)+(3+4");
     }
 
     @Test(expected = BracketsException.class)
-    public void test_noOpeningBracket_getExpression() throws Exception
+    public void test_noOpeningBracket_validateExpression() throws Exception
     {
         this.validateExpression("3+5)");
     }
 
     @Test(expected = BracketsException.class)
-    public void test_notEnoughClosingBrackets_getExpression() throws Exception
+    public void test_notEnoughClosingBrackets_validateExpression() throws Exception
     {
         this.validateExpression("(()");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_emptyBrackets_getExpression() throws Exception
+    public void test_emptyBrackets_validateExpression() throws Exception
     {
         this.validateExpression("1+()");
     }
 
     @Test(expected = OperatorMisplacementException.class)
-    public void test_singleOperatorWithinBrackets_getExpression() throws Exception
+    public void test_singleOperatorWithinBrackets_validateExpression() throws Exception
     {
         this.validateExpression("(+)");
     }
