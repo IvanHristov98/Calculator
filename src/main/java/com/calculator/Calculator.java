d0becf30a87a5ed6b2467d42b1fb8179d4056d34
Hristov <ivan.hristov@sap.com>
2019-07-10 15:43:33 +0300

    Clear Calculator class and change methods to stub ones.

--- src/main/java/com/calculator/Calculator.java
+++ src/main/java/com/calculator/Calculator.java
@@ -1,36 +1,23 @@
 package com.calculator;
 
 public class Calculator
-{	
-	private MathematicalExpressionParser parser;
-	
-	private void init(String expression) throws Exception
-	{
-		this.parser = MathematicalExpressionParser.constructFromExpression(expression);
-	}
-	
+{
 	private Calculator()
 	{}
 	
 	private Calculator(String expression) throws Exception
 	{
-		this.init(expression);
+
 	}
 	
 	public static Calculator constructFromExpression(String expression) throws Exception
 	{
 		return new Calculator(expression);
 	}
-	
-	public void changeExpression(String expression) throws Exception
-	{
-		this.init(expression);
-	}
-	
+
 	public Double calculate() throws Exception
 	{
-		CalculationUnit  calcUnit = CalculationUnit.constructFromParser(this.parser.getRPExpression());
-		
-		return calcUnit.getCalculationResult();
+		// stub
+		return 0.0d;
 	}
 }
