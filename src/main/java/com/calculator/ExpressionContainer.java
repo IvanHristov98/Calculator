e823ad8a26eaecacbcc104dd8d7568aa6d89036f
Hristov <ivan.hristov@sap.com>
2019-07-10 11:12:33 +0300

    Add ExpressionContainer functionality.

--- src/main/java/com/calculator/ExpressionContainer.java
+++ src/main/java/com/calculator/ExpressionContainer.java
@@ -1,4 +1,41 @@
 package com.calculator;
 
-public class ExpressionContainer {
+import com.calculator.exception.*;
+
+public abstract class ExpressionContainer
+{
+    protected Expression expression;
+
+    protected ExpressionContainer()
+    {}
+
+    protected ExpressionContainer(Expression expression)
+    {
+        this.setExpression(expression);
+    }
+
+    public void setExpression(Expression expression)
+    {
+        this.expression = expression;
+    }
+
+    public Expression getExpression()
+    {
+        return this.expression.clone();
+    }
+
+    protected String stripSpaces(String expression)
+    {
+        return expression.replaceAll(" ", "");
+    }
+
+    public static ExpressionValidator makeValidatorFromExpression(Expression expression)
+    {
+        return new ExpressionValidator(expression);
+    }
+
+    public static ExpressionParser makeParserFromExpression(Expression expression)
+    {
+        return new ExpressionParser(expression);
+    }
 }
