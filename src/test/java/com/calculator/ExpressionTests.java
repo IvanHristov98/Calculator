1833b01aff9966906295cd740e251d105053078b
Hristov <ivan.hristov@sap.com>
2019-07-10 15:59:36 +0300

    Change assertions in ExpressionTests class.

--- src/test/java/com/calculator/ExpressionTests.java
+++ src/test/java/com/calculator/ExpressionTests.java
@@ -1,41 +1,42 @@
 package com.calculator;
 
 import org.junit.Test;
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNull;
 
-public class ExpressionTests 
+public class ExpressionTests
 {
 	@Test
 	public void test_constructEmptyExpression()
 	{
 		Expression expression = Expression.constructEmptyExpression();
-		
-		assertTrue(expression.getContent() == null);
+
+		assertNull(expression.getContent());
 	}
-	
+
 	@Test
 	public void test_constructFromExpression()
 	{
 		Expression expression = Expression.constructFromExpressionContent("1+1");
-		
-		assertTrue("1+1".equals(expression.getContent()));
+
+		assertEquals("1+1", expression.getContent());
 	}
-	
+
 	@Test
 	public void test_setContent()
 	{
 		Expression expression = Expression.constructEmptyExpression();
 		expression.setContent("1+1");
-		
-		assertTrue("1+1".equals(expression.getContent()));
+
+		assertEquals("1+1", expression.getContent());
 	}
-	
+
 	@Test
 	public void test_clone()
 	{
 		Expression expression = Expression.constructFromExpressionContent("1+1");
 		Expression clonedExpression = expression.clone();
-		
-		assertTrue(expression.equals(clonedExpression));
+
+		assertEquals(expression, clonedExpression);
 	}
 }
