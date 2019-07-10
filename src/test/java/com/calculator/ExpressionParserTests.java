e239176ddb6ae71227b1f1f32ceca1f10e686f7c
Hristov <ivan.hristov@sap.com>
2019-07-10 15:29:03 +0300

    Change from assertTrue to assertEquals in ExpressionParserTests class.

--- src/test/java/com/calculator/ExpressionParserTests.java
+++ src/test/java/com/calculator/ExpressionParserTests.java
@@ -2,50 +2,50 @@ package com.calculator;
 
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertEquals;
 
 public class ExpressionParserTests 
 {
 	@Test
 	public void test_condensed_getExpression() throws Exception
 	{
-		assertTrue("( 3 + 4 + 5 )".equals(this.getParsedExpression("3+4+5")));
+		assertEquals("( 3 + 4 + 5 )", this.getParsedExpression("3+4+5"));
 	}
 	
 	@Test
 	public void test_condensedBrackets_getExpression() throws Exception
 	{
-		assertTrue("( 3 * ( 4 + 5 ) )".equals(this.getParsedExpression("3*(4+5)")));
+		assertEquals("( 3 * ( 4 + 5 ) )", this.getParsedExpression("3*(4+5)"));
 	}
 	
 	@Test
 	public void test_floatingPointNumbers_getExpression() throws Exception
 	{
-		assertTrue("( 3.5 + 123.4567 )".equals(this.getParsedExpression("3.5+123.4567")));
+		assertEquals("( 3.5 + 123.4567 )", this.getParsedExpression("3.5+123.4567"));
 	}
 	
 	@Test
 	public void test_sparseExpression_getExpression() throws Exception
 	{
-		assertTrue("( 1 + 1 + 2 )".equals(this.getParsedExpression("1 +1+ 2")));
+		assertEquals("( 1 + 1 + 2 )", this.getParsedExpression("1 +1+ 2"));
 	}
 	
 	@Test
 	public void test_misplacedRedundantOperatorAtStart_getExpression() throws Exception
 	{
-		assertTrue("( 1 / 2 )".equals(this.getParsedExpression("+1/2")));
+		assertEquals("( 1 / 2 )", this.getParsedExpression("+1/2"));
 	}
 	
 	@Test
 	public void test_minusAtBeginning_getExpression() throws Exception
 	{
-		assertTrue("( -1 + 2 )".equals(this.getParsedExpression("-1+2")));
+		assertEquals("( -1 + 2 )", this.getParsedExpression("-1+2"));
 	}
 
 	@Test
 	public void test_minusStraightAfterBracket_getExpression() throws Exception
 	{
-		assertTrue("( ( -1 + 1 ) )".equals(this.getParsedExpression("(-1+1)")));
+		assertEquals("( ( -1 + 1 ) )", this.getParsedExpression("(-1+1)"));
 	}
 
 	private String getParsedExpression(String content) throws Exception
