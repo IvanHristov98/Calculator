75a9669790d6a30213c59a36133225e79bcdb7ef
Hristov <ivan.hristov@sap.com>
2019-07-11 15:44:07 +0300

    Remove redundant Expression tests.

--- src/test/java/com/calculator/ExpressionTests.java
+++ src/test/java/com/calculator/ExpressionTests.java
@@ -2,27 +2,9 @@ package com.calculator;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
-import static org.junit.Assert.assertNull;
 
 public class ExpressionTests
 {
-	@Test
-	public void test_constructFromExpression()
-	{
-		Expression expression = new Expression("1+1");
-
-		assertEquals("1+1", expression.getContent());
-	}
-
-	@Test
-	public void test_setContent()
-	{
-		Expression expression = new Expression("2+2");
-		expression.setContent("1+1");
-
-		assertEquals("1+1", expression.getContent());
-	}
-
 	@Test
 	public void test_clone()
 	{
