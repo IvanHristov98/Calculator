92c4e108c21eae740e989fb2125f9cd23a56a9b2
Hristov <ivan.hristov@sap.com>
2019-07-11 11:00:14 +0300

    Add getConversedExpression tests to ShuntingYardParserTests class.

--- src/test/java/com/calculator/ShuntingYardParserTests.java
+++ src/test/java/com/calculator/ShuntingYardParserTests.java
@@ -24,4 +24,78 @@ public class ShuntingYardParserTests
 
         assertEquals(expression, parser.getExpression());
     }
+
+    @Test
+    public void test__twoOperators_getConversedExpression() throws Exception
+    {
+        assertEquals("1 1 +", this.getConversedExpression("( 1 + 1 )"));
+    }
+
+    @Test
+    public void test__expressionWithoutBrackets_getConversedExpression() throws Exception
+    {
+        assertEquals("1 1 +", this.getConversedExpression("1 + 1"));
+    }
+
+    @Test
+    public void test_negativeNumbers_getConversedExpression() throws Exception
+    {
+        assertEquals("-1 1 +", this.getConversedExpression("-1 + 1"));
+    }
+
+    @Test
+    public void test_priorityOfOperators_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 * 3 +", this.getConversedExpression("1 * 2 + 3"));
+    }
+
+    @Test
+    public void test_leftSideAssociativity_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 + 3 *", this.getConversedExpression("( 1 + 2 ) * 3"));
+    }
+
+    @Test
+    public void test_rightSideAssociativity_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 3 + *", this.getConversedExpression("1 * ( 2 + 3 )"));
+    }
+
+    @Test
+    public void test_multipleBrackets_getConversedExpression() throws Exception
+    {
+        assertEquals("1 1 +", this.getConversedExpression("( ( ( 1 + 1 ) ) )"));
+    }
+
+    @Test
+    public void test_productOfTwoBracketedExpression_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 + 3 4 + *", this.getConversedExpression("( 1 + 2 ) * ( 3 + 4 )"));
+    }
+
+    @Test
+    public void test_singlePowOfABracketedExpression_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 + 3 ^", this.getConversedExpression("( 1 + 2 ) ^ 3"));
+    }
+
+    @Test
+    public void test_multiplePows_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 3 4 ^ ^ ^", this.getConversedExpression("1 ^ 2 ^ 3 ^ 4"));
+    }
+
+    @Test
+    public void test_expressionWithinBracketsBetweenPows_getConversedExpression() throws Exception
+    {
+        assertEquals("1 2 3 + 4 ^ ^", this.getConversedExpression("1 ^ ( 2 + 3 ) ^ 4"));
+    }
+
+    public String getConversedExpression(String expressionContent) throws Exception
+    {
+        Expression expression = Expression.constructFromExpressionContent(expressionContent);
+        ShuntingYardParser parser = ExpressionContainer.makeShuntingYardParserFromExpression(expression);
+
+        return parser.getConvertedExpression().getContent();
+    }
 }
