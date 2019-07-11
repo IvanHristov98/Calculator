d17c1a34d3d4882982ff107390bc532bb464627e
Hristov <ivan.hristov@sap.com>
2019-07-11 14:19:30 +0300

    Add class with predicates for operator types.

---
+++ src/main/java/com/calculator/operator/OperatorChecker.java
@@ -0,0 +1,38 @@
+package com.calculator.operator;
+
+public class OperatorChecker
+{
+
+    public static boolean isPlus(Operator operator)
+    {
+        return operator instanceof PlusOperator;
+    }
+
+    public static boolean isMinus(Operator operator)
+    {
+        return operator instanceof MinusOperator;
+    }
+
+    public static boolean isProduct(Operator operator)
+    {
+        return operator instanceof ProductOperator;
+    }
+
+    public static boolean isDivision(Operator operator)
+    {
+        return operator instanceof DivisionOperator;
+    }
+
+    public static boolean isLeftBracket(Operator operator)
+    {
+        return operator instanceof LeftBracketOperator;
+    }
+
+    public static boolean isRightBracket(Operator operator)
+    {
+        return operator instanceof RightBracketOperator;
+    }
+
+    private OperatorChecker()
+    {}
+}
