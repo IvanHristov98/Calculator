505585cf97ca894ecd784ffe74d744388f3e247b
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-22 14:33:16 +0300

    Rename function arguments into UnformattedInfixExpressionValidator.

--- calculator-core/src/main/java/com/calculator/core/UnformattedInfixExpressionValidator.java
+++ calculator-core/src/main/java/com/calculator/core/UnformattedInfixExpressionValidator.java
@@ -29,73 +29,73 @@ public class UnformattedInfixExpressionValidator extends ExpressionContainer
         return this.expression;
     }
 
-    private void validateIfAnyConsecutiveNumbers(String expression) throws NumberMisplacementException
+    private void validateIfAnyConsecutiveNumbers(String expressionContent) throws NumberMisplacementException
     {
-        if (this.hasConsecutiveNumbers(expression))
+        if (this.hasConsecutiveNumbers(expressionContent))
         {
             throw new NumberMisplacementException("Invalid expression. Numbers should be separated by a valid operator.");
         }
     }
 
-    private boolean hasConsecutiveNumbers(String expression)
+    private boolean hasConsecutiveNumbers(String expressionContent)
     {
-        return expression.matches(".*[0-9.][ ]+[0-9.].*");
+        return expressionContent.matches(".*[0-9.][ ]+[0-9.].*");
     }
 
-    private void validateNumbersFormat(String expression) throws NumberMisplacementException
+    private void validateNumbersFormat(String expressionContent) throws NumberMisplacementException
     {
-        if (this.hasAnInvalidValidNumber(expression))
+        if (this.hasAnInvalidNumber(expressionContent))
         {
             throw new NumberMisplacementException("The given expression contains invalid numbers.");
         }
     }
 
-    private boolean hasAnInvalidValidNumber(String expression)
+    private boolean hasAnInvalidNumber(String expressionContent)
     {
         // checks if there are two floating point numbers one after another
         // following the mantissa_1.exponent_1mantissa_2.exponent_2
-        return Pattern.matches(".*[0-9]+\\.[0-9]*\\..*", expression);
+        return Pattern.matches(".*[0-9]+\\.[0-9]*\\..*", expressionContent);
     }
 
-    private void validateIfExpressionHasEmptySubexpressions(String expression) throws EmptyExpressionException
+    private void validateIfExpressionHasEmptySubexpressions(String expressionContent) throws EmptyExpressionException
     {
-        if (expression.contains("()"))
+        if (expressionContent.contains("()"))
         {
             throw new EmptyExpressionException("An empty expression is not a valid one.");
         }
     }
 
-    private void validateOperatorSequence(String expression) throws OperatorMisplacementException
+    private void validateOperatorSequence(String expressionContent) throws OperatorMisplacementException
     {
-        if (this.hasConsecutiveOperators(expression))
+        if (this.hasConsecutiveOperators(expressionContent))
         {
             throw new OperatorMisplacementException("The given expression contains consecutive operators.");
         }
     }
 
-    private boolean hasConsecutiveOperators(String expression)
+    private boolean hasConsecutiveOperators(String expressionContent)
     {
         // There should be no two consecutive operators -, +, *, / or ^
-        return  Pattern.matches(".*[-+*/^]{2,}.*", expression);
+        return  Pattern.matches(".*[-+*/^]{2,}.*", expressionContent);
     }
 
-    private void validateTokens(String expression) throws InvalidOperatorException
+    private void validateTokens(String expressionContent) throws InvalidOperatorException
     {
-        if (this.hasInvalidTokens(expression))
+        if (this.hasInvalidTokens(expressionContent))
         {
             throw new InvalidOperatorException("The given expression contains invalid tokens.");
         }
     }
 
-    private boolean hasInvalidTokens(String expression)
+    private boolean hasInvalidTokens(String expressionContent)
     {
         // An expression should only contain the symbols -, +, *, /, ^, (, ), .,  0-9
-        return Pattern.matches(".*[^-+*/^()0-9.].*", expression);
+        return Pattern.matches(".*[^-+*/^()0-9.].*", expressionContent);
     }
 
-    private void validateIfAnyNumbersAreGluedAroundBracketedExpression(String expression) throws BracketsException
+    private void validateIfAnyNumbersAreGluedAroundBracketedExpression(String expressionContent) throws BracketsException
     {
-        if (this.hasNumbersGluedAroundBracketExpression(expression))
+        if (this.hasNumbersGluedAroundBracketExpression(expressionContent))
         {
             throw new BracketsException(
                     "It is not allowed to have a number right before a '(' symbol or right after a ')' symbol."
@@ -103,10 +103,10 @@ public class UnformattedInfixExpressionValidator extends ExpressionContainer
         }
     }
 
-    private boolean hasNumbersGluedAroundBracketExpression(String expression)
+    private boolean hasNumbersGluedAroundBracketExpression(String expressionContent)
     {
         // Prevents having expressions with the format number1([sub_expression])number2
-        return expression.matches(".*([0-9.]\\(|\\)[0-9]).*");
+        return expressionContent.matches(".*([0-9.]\\(|\\)[0-9]).*");
     }
     
     private void validateSubexpressionEnds(String expressionContent) throws CalculatorException
@@ -115,21 +115,21 @@ public class UnformattedInfixExpressionValidator extends ExpressionContainer
         this.validateExpressionBeforeClosingBrackets(expressionContent);
     }
     
-    private void validateExpressionAfterOpeningBrackets(String expression) throws OperatorMisplacementException
+    private void validateExpressionAfterOpeningBrackets(String expressionContent) throws OperatorMisplacementException
     {
     	// there shouldn't be an *, / or ^ symbol right after an opening bracket
-    	this.validateStringByPattern(expression, ".*\\([*\\/^].*");
+    	this.validateStringByPattern(expressionContent, ".*\\([*\\/^].*");
     }
 
-    private void validateExpressionBeforeClosingBrackets(String expression) throws OperatorMisplacementException
+    private void validateExpressionBeforeClosingBrackets(String expressionContent) throws OperatorMisplacementException
     {
     	// A there should be no arithmetic operators right before a closing bracket
-    	this.validateStringByPattern(expression, ".*[-+*\\/^]\\).*");
+    	this.validateStringByPattern(expressionContent, ".*[-+*\\/^]\\).*");
     }
     
-    private void validateStringByPattern(String expression, String invalidPattern) throws OperatorMisplacementException
+    private void validateStringByPattern(String string, String invalidPattern) throws OperatorMisplacementException
     {
-        if (Pattern.matches(invalidPattern, expression))
+        if (Pattern.matches(invalidPattern, string))
         {
             throw new OperatorMisplacementException(
                     "Expression is not ordered properly."
