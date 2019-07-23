3f2218339ff368e9f827d0a9b9f7c5e7aa06df29
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-23 13:06:09 +0300

    Update CalculatorTest.

--- calculator-core/src/test/java/com/calculator/core/CalculatorTest.java
+++ calculator-core/src/test/java/com/calculator/core/CalculatorTest.java
@@ -1,95 +1,56 @@
 package com.calculator.core;
 
-import com.calculator.core.exception.*;
-
 import org.junit.Test;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.calculator.core.exception.CalculatorException;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.times;
+
 import static org.junit.Assert.assertEquals;
+
+import org.junit.Before;
 
 public class CalculatorTest
 {	
-	private static final double ALLOWED_ERROR = 0.01;
+	@Mock
+	public Expression expression;
+	@Mock
+	public UnformattedInfixExpressionValidator infixValidator;
+	@Mock
+	public InfixExpressionFormatUnifier infixFormatter;
+	@Mock
+	public InfixToPostfixExpressionTranslator postfixTranslator;
+	@Mock
+	public PostfixExpressionCalculator postfixCalculator;
 	
-	private double calculate(String expressionContent) throws Exception
+	@Before
+	public void setUp()
 	{
-		Expression expression = new Expression(expressionContent);
-		UnformattedInfixExpressionValidator infixValidator = new UnformattedInfixExpressionValidator(expression);
-		InfixExpressionFormatUnifier infixFormatter = new InfixExpressionFormatUnifier(expression);
-		InfixToPostfixExpressionTranslator postfixTranslator = new InfixToPostfixExpressionTranslator(expression);
-		PostfixExpressionCalculator postfixCalculator = new PostfixExpressionCalculator(expression);
+		MockitoAnnotations.initMocks(this);
+	}
+	
+	@Test
+	public void calculationSequence_calculate() throws CalculatorException
+	{
+		when(postfixCalculator.process()).thenReturn(new Expression("1"));
 		
-		Calculator calc  = new Calculator(
-				expression, 
-				infixValidator,
-				infixFormatter,
-				postfixTranslator,
-				postfixCalculator
+		Calculator calculator = new Calculator(
+				this.expression,
+				this.infixValidator,
+				this.infixFormatter,
+				this.postfixTranslator,
+				this.postfixCalculator
 				);
-		return calc.calculate();
-	}
-	
-	@Test
-	public void sumAndSubtractMultipleNumbers_calculate() throws Exception
-	{
-		assertEquals(5.8, this.calculate("3.5+2.3+3-3"), CalculatorTest.ALLOWED_ERROR);
-	}
-	
-	@Test(expected = OperatorMisplacementException.class)
-	public void multiplyNegativeNumbers_calculate() throws Exception
-	{
-		assertEquals(1, this.calculate("-1*-1"), 0);
-	}
-
-	@Test(expected = DivisionByZeroException.class)
-	public void divideByZero_calculate() throws Exception
-	{
-		this.calculate("1/0");
-	}
-	
-	@Test
-	public void associativityWithBrackets_calculate() throws Exception
-	{
-		assertEquals(8, this.calculate("2+(3/2)*4"), CalculatorTest.ALLOWED_ERROR);
-	}
-	
-	@Test(expected = BracketsException.class)
-	public void missedOpeningBracket_calculate() throws Exception
-	{
-		this.calculate("3+4)/2");
-	}
-	
-	@Test(expected = BracketsException.class)
-	public void missedClosingBracket_calculate() throws Exception
-	{
-		this.calculate("(3+4/2");
-	}
-	
-	@Test(expected = OperatorMisplacementException.class)
-	public void operatorCount_calculate() throws Exception
-	{
-		this.calculate("3++5/2");
-	}
-	
-	@Test(expected = OperatorMisplacementException.class)
-	public void operatorMisplacementOnEachSide_calculate() throws Exception
-	{
-		this.calculate("*3+5+");
-	}
-	
-	@Test(expected = InvalidOperatorException.class)
-	public void letterInsteadOfNumber_calculate() throws Exception
-	{
-		this.calculate("5A5+3");
-	}
-	
-	@Test(expected = NumberMisplacementException.class)
-	public void spacedNumbers() throws Exception
-	{
-		this.calculate("5 4+6");
-	}
-	
-	@Test
-	public void spacedExpression_calculate() throws Exception
-	{
-		assertEquals(17, this.calculate("1 + 2*(3 +5)"), CalculatorTest.ALLOWED_ERROR);
+		
+		calculator.calculate();
+		
+		verify(this.infixValidator, times(1)).process();
+		verify(this.infixFormatter, times(1)).process();
+		verify(this.postfixTranslator, times(1)).process();
+		verify(this.postfixCalculator, times(1)).process();
 	}
 }
