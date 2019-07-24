1aa5748e025b4b59b27d303e4b63f53aa7d6159c
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-24 14:27:49 +0300

    Cover Expression functionality with tests.

--- calculator-core/src/test/java/com/calculator/core/ExpressionTest.java
+++ calculator-core/src/test/java/com/calculator/core/ExpressionTest.java
@@ -1,7 +1,9 @@
 package com.calculator.core;
 
 import org.junit.Test;
+
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertArrayEquals;
 
 public class ExpressionTest
 {
@@ -12,5 +14,31 @@ public class ExpressionTest
 		Expression clonedExpression = expression.clone();
 
 		assertEquals(expression, clonedExpression);
+	}
+	
+	@Test
+	public void testGetTokens()
+	{
+		Expression expression = new Expression("( 1 + 2 ) * 3");
+		
+		assertArrayEquals(new String[] {"(", "1", "+", "2", ")", "*", "3"}, expression.getTokens());
+	}
+	
+	@Test
+	public void testWrapWithBrackets()
+	{
+		Expression expression = new Expression("1+2");
+		expression.wrapWithBrackets();
+		
+		assertEquals("(1+2)", expression.getContent());
+	}
+	
+	@Test
+	public void stripContentSpaces()
+	{
+		Expression expression = new Expression("1 + 1");
+		expression.stripContentSpaces();
+		
+		assertEquals("1+1", expression.getContent());
 	}
 }
