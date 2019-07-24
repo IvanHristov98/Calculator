d55a99ae62658628cb31e7678140ded9c59d32c1
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-24 10:59:38 +0300

    Modify CalculatorTest.

--- calculator-core/src/test/java/com/calculator/core/CalculatorTest.java
+++ calculator-core/src/test/java/com/calculator/core/CalculatorTest.java
@@ -1,16 +1,22 @@
 package com.calculator.core;
 
-import org.junit.Test;
-import org.mockito.Mock;
-import org.mockito.MockitoAnnotations;
-
 import com.calculator.core.exception.CalculatorException;
 
+import org.junit.Test;
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.Mockito;
+import org.mockito.MockitoAnnotations;
+
 import static org.mockito.Mockito.when;
-import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verify;
 
 import org.junit.Before;
+
+import static org.junit.Assert.assertThat;
+import static org.hamcrest.CoreMatchers.equalTo;
 
 public class CalculatorTest
 {	
@@ -24,31 +30,58 @@ public class CalculatorTest
 	public InfixToPostfixExpressionTranslator postfixTranslator;
 	@Mock
 	public PostfixExpressionCalculator postfixCalculator;
+	@Mock
+	public Calculator calculator;
 	
 	@Before
 	public void setUp()
 	{
 		MockitoAnnotations.initMocks(this);
-	}
-	
-	@Test
-	public void calculationSequence_calculate() throws CalculatorException
-	{
-		when(postfixCalculator.process()).thenReturn(new Expression("1"));
 		
-		Calculator calculator = new Calculator(
+		this.calculator = new Calculator(
 				this.expression,
 				this.infixValidator,
 				this.infixFormatter,
 				this.postfixTranslator,
 				this.postfixCalculator
 				);
+	}
+	
+	@Test
+	public void verifyOrderOfExpressionContainerProcessCalls_calculate() throws CalculatorException
+	{
+		// prevents NullPointerException
+		when(this.postfixCalculator.process()).thenReturn(new Expression("1"));
 		
-		calculator.calculate();
+		this.calculator.calculate();
+		
+		InOrder mockOrder = inOrder(this.infixValidator, this.infixFormatter, this.postfixTranslator, this.postfixCalculator);
+		
+		mockOrder.verify(this.infixValidator).process();
+		mockOrder.verify(this.infixFormatter).process();
+		mockOrder.verify(this.postfixTranslator).process();
+		mockOrder.verify(this.postfixCalculator).process();
+	}
+	
+	@Test
+	public void verifyNumberOfExpressionContainerProcessCalss_calculae() throws CalculatorException
+	{
+		// prevents NullPointerException
+		when(this.postfixCalculator.process()).thenReturn(new Expression("1"));
+		
+		this.calculator.calculate();
 		
 		verify(this.infixValidator, times(1)).process();
 		verify(this.infixFormatter, times(1)).process();
 		verify(this.postfixTranslator, times(1)).process();
 		verify(this.postfixCalculator, times(1)).process();
+	}
+	
+	@Test
+	public void verifyCorrectCalculation_calculate() throws CalculatorException
+	{
+		when(this.postfixCalculator.process()).thenReturn(new Expression("1"));
+		
+		assertThat(this.calculator.calculate(), equalTo(Double.valueOf(1.0d)));
 	}
 }
