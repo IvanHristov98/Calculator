765feec7b34e2c02c431dd453e78cd820312c21f
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-24 13:13:50 +0300

    Fix InfixExpressionFormatUnifierTests.
    Mock Expression dependencies.

--- calculator-core/src/test/java/com/calculator/core/InfixExpressionFormatUnifierTest.java
+++ calculator-core/src/test/java/com/calculator/core/InfixExpressionFormatUnifierTest.java
@@ -1,58 +1,101 @@
 package com.calculator.core;
 
+import org.mockito.InOrder;
+import org.mockito.Mock;
+import org.mockito.MockitoAnnotations;
+
+import com.calculator.core.exception.CalculatorException;
+
+import static org.mockito.Mockito.when;
+import static org.mockito.Mockito.inOrder;
+import static org.mockito.Mockito.verifyNoMoreInteractions;
+
 import org.junit.Test;
+import org.junit.Before;
 
 import static org.junit.Assert.assertEquals;
 
 public class InfixExpressionFormatUnifierTest
 {
-	@Test
-	public void condensed_process() throws Exception
-	{
-		assertEquals("( 3 + 4 + 5 )", this.getParsedExpression("3+4+5"));
-	}
+	@Mock
+	public Expression expression;
+	InfixExpressionFormatUnifier formatUnifier;
 	
-	@Test
-	public void condensedBrackets_process() throws Exception
+	@Before
+	public void setUp()
 	{
-		assertEquals("( 3 * ( 4 + 5 ) )", this.getParsedExpression("3*(4+5)"));
-	}
-	
-	@Test
-	public void floatingPointNumbers_process() throws Exception
-	{
-		assertEquals("( 3.5 + 123.4567 )", this.getParsedExpression("3.5+123.4567"));
-	}
-	
-	@Test
-	public void sparseExpression_process() throws Exception
-	{
-		assertEquals("( 1 + 1 + 2 )", this.getParsedExpression("1 +1+ 2"));
-	}
-	
-	@Test
-	public void redundantOperatorAtStart_process() throws Exception
-	{
-		assertEquals("( 1 / 2 )", this.getParsedExpression("+1/2"));
-	}
-	
-	@Test
-	public void minusStraightAfterBracket_process() throws Exception
-	{
-		assertEquals("( ( -1 + 1 ) )", this.getParsedExpression("(-1+1)"));
-	}
-	
-	@Test
-	public void minusAtBeginning_process() throws Exception
-	{
-		assertEquals("( -1 + 2 )", this.getParsedExpression("-1+2"));
-	}
-
-	private String getParsedExpression(String content) throws Exception
-	{
-		Expression exp = new Expression(content);
-		InfixExpressionFormatUnifier parser = new InfixExpressionFormatUnifier(exp);
+		MockitoAnnotations.initMocks(this);
 		
-		return parser.process().getContent();
+		this.formatUnifier = new InfixExpressionFormatUnifier(this.expression);
+	}
+	
+	@Test
+	public void verifyOrderOfExpressionMethodCalls_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("( 1 )");
+		
+		this.formatUnifier.process();
+		
+		InOrder mockOrder = inOrder(this.expression);
+		
+		mockOrder.verify(this.expression).wrapWithBrackets();
+		mockOrder.verify(this.expression).stripContentSpaces();
+		mockOrder.verify(this.expression).getContent();
+		
+		verifyNoMoreInteractions(this.expression);
+	}
+	
+	@Test
+	public void condensed_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("(3+4+5)");
+		
+		Expression resultExpression = this.formatUnifier.process();
+		assertEquals("( 3 + 4 + 5 )", resultExpression.getContent());
+	}
+	
+	@Test
+	public void condensedBrackets_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("(3*(4+5))");
+		
+		Expression resultExpression = this.formatUnifier.process();
+		assertEquals("( 3 * ( 4 + 5 ) )", resultExpression.getContent());
+	}
+	
+	@Test
+	public void floatingPointNumbers_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("(3.5+123.4567)");
+		
+		Expression resultExpression = this.formatUnifier.process();
+		assertEquals("( 3.5 + 123.4567 )", resultExpression.getContent());
+	}
+	
+	@Test
+	public void redundantOperatorAtStart_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("(+1/2)");
+		
+		Expression resultExpression = this.formatUnifier.process();
+		assertEquals("( 1 / 2 )", resultExpression.getContent());
+	}
+	
+	@Test
+	public void minusStraightAfterBracket_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("((-1+1))");
+		
+		Expression resultExpression = this.formatUnifier.process();
+		assertEquals("( ( -1 + 1 ) )", resultExpression.getContent());
+	}
+	
+	@Test
+	public void minusAtBeginning_process() throws CalculatorException
+	{
+		when(this.expression.getContent()).thenReturn("(-1+2)");
+		
+		Expression resultExpression = this.formatUnifier.process();
+		assertEquals("( -1 + 2 )", resultExpression.getContent());
 	}
 }
