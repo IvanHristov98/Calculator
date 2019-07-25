99fec7eb39988003c23d9534f9bb6e0d285c3020
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-25 08:57:05 +0300

    Add UnformattedExpressionException.

---
+++ calculator-core/src/main/java/com/calculator/core/exception/UnformattedExpressionException.java
@@ -0,0 +1,10 @@
+package com.calculator.core.exception;
+
+public class UnformattedExpressionException extends CalculatorException {
+
+	private static final long serialVersionUID = 1L;
+
+	public UnformattedExpressionException(String message) {
+		super(message);
+	}
+}
