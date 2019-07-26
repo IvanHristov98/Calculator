ca6d0cb42784707f0937886b3391bf78f0e14fb7
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-26 13:40:42 +0300

    Wrap calculator-core exception in wrapper class.

--- calculator-cli/src/main/java/com/calculator/cli/coreWrapper/CalculatorWrapper.java
+++ calculator-cli/src/main/java/com/calculator/cli/coreWrapper/CalculatorWrapper.java
@@ -1,7 +1,7 @@
 package com.calculator.cli.coreWrapper;
 
 import com.calculator.core.*;
-import com.calculator.core.exception.CalculatorException;
+import com.calculator.core.exception.*;
 
 public class CalculatorWrapper {
 	private String expressionContent;
@@ -12,7 +12,31 @@ public class CalculatorWrapper {
 		this.calculatorSupplier = calculatorSupplier;
 	}
 	
-	public double calculate() throws CalculatorException {
+	public double calculate() throws Exception {
+		try {
+			return this.getCalculationResult();
+		}
+		catch (BracketsException exception) {
+			throw new Exception("Expression error. Brackets error encountered.");
+		}
+		catch (OperatorMisplacementException exception) {
+			throw new Exception("Expression error. Operator misplacement error encountered.");
+		}
+		catch (InvalidOperatorException exception) {
+			throw new Exception("Expression error. Invalid operators encountered. Valid operator symbols are +, -, *, / and ^.");
+		}
+		catch (DivisionByZeroException exception) {
+			throw new Exception("Expression error. Division by zero encountered.");
+		}
+		catch (EmptyExpressionException exception) {
+			throw new Exception("Expression error. Empty expression encountered.");
+		}
+		catch (NumberMisplacementException exception) {
+			throw new Exception("Expression error. Number misplacement encountered. Numbers should be separated by arithmetic operators.");
+		}
+	}
+	
+	public double getCalculationResult() throws CalculatorException {
 		Calculator calculator = this.calculatorSupplier.supplyCalculator(this.expressionContent);
 		
 		return calculator.calculate();
--- calculator-cli/src/test/java/com/calculator/cli/coreWrapper/CalculatorWrapperTest.java
+++ calculator-cli/src/test/java/com/calculator/cli/coreWrapper/CalculatorWrapperTest.java
@@ -1,15 +1,19 @@
 package com.calculator.cli.coreWrapper;
 
 import com.calculator.core.Calculator;
-import com.calculator.core.exception.CalculatorException;
+import com.calculator.core.exception.*;
 
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 
 import static org.mockito.Mockito.when;
+import static org.mockito.ArgumentMatchers.anyString;
 
 import org.junit.Before;
+import org.junit.Rule;
+
 import org.junit.Test;
+import org.junit.rules.ExpectedException;
 
 import static org.junit.Assert.assertEquals;
 
@@ -20,19 +24,85 @@ public class CalculatorWrapperTest {
 	Calculator calculator;
 	CalculatorWrapper calculatorWrapper;
 	
+	@Rule
+	public ExpectedException expectedException = ExpectedException.none();
+	
 	@Before
 	public void setUp() {
 		MockitoAnnotations.initMocks(this);
 	}
 	
 	@Test
-	public void getCalculationResult_calculate() throws CalculatorException {
+	public void returnOfCorrectCalculationResult_calculate() throws Exception {
 		String expressionContent = "1";
+		
 		when(this.calculatorSupplier.supplyCalculator(expressionContent)).thenReturn(this.calculator);
 		when(this.calculator.calculate()).thenReturn(1.0d);
 		
-		this.calculatorWrapper = new CalculatorWrapper("1", this.calculatorSupplier);
+		this.calculatorWrapper = new CalculatorWrapper(expressionContent, this.calculatorSupplier);
 		
 		assertEquals(1.0d, this.calculatorWrapper.calculate(), 0.0001);
+	}
+	
+	@Test
+	public void verifyOperatorMisplacementException_calculate() throws Exception {
+		this.verifyCalculatorException(
+				"Expression error. Operator misplacement error encountered.",
+				OperatorMisplacementException.class
+				);
+	}
+	
+	@Test
+	public void verifyBracketsException_calculate() throws Exception {
+		this.verifyCalculatorException(
+				"Expression error. Brackets error encountered.",
+				BracketsException.class
+				);
+	}
+	
+	@Test
+	public void verifyDivisionByZeroException_calculate() throws Exception {
+		this.verifyCalculatorException(
+				"Expression error. Division by zero encountered.",
+				DivisionByZeroException.class
+				);
+	}
+	
+	@Test
+	public void verifyEmptyExpressionException_calculate() throws Exception {
+		this.verifyCalculatorException(
+				"Expression error. Empty expression encountered.",
+				EmptyExpressionException.class
+				);
+	}
+	
+	@Test
+	public void verifyInvalidOperatorException_calculate() throws Exception {
+		this.verifyCalculatorException(
+				"Expression error. Invalid operators encountered. Valid operator symbols are +, -, *, / and ^.",
+				InvalidOperatorException.class
+				);
+	}
+	
+	@Test
+	public void verifyNumberMisplacementException() throws Exception {
+		this.verifyCalculatorException(
+				"Expression error. Number misplacement encountered. Numbers should be separated by arithmetic operators.",
+				NumberMisplacementException.class
+				);
+	}
+	
+	private void verifyCalculatorException(String message, Class<? extends Throwable> exceptionToMockWith)
+			throws Exception {
+		this.expectedException.expect(Exception.class);
+		this.expectedException.expectMessage(message);
+		
+		String expressionContent = "1";
+		
+		when(this.calculatorSupplier.supplyCalculator(anyString())).thenReturn(this.calculator);
+		when(this.calculator.calculate()).thenThrow(exceptionToMockWith);
+		
+		this.calculatorWrapper = new CalculatorWrapper(expressionContent, this.calculatorSupplier);
+		this.calculatorWrapper.calculate();
 	}
 }
