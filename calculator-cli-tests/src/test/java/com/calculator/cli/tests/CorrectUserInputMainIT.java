d9e0bbc2c8df119ae4227249e55dd481e717e158
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-30 15:26:48 +0300

    Modify CorrectUserInputMainIT to now test the cli jar file.

--- calculator-cli-tests/src/test/java/com/calculator/cli/tests/CorrectUserInputMainIT.java
+++ calculator-cli-tests/src/test/java/com/calculator/cli/tests/CorrectUserInputMainIT.java
@@ -1,40 +1,26 @@
-	package com.calculator.cli.tests;
+package com.calculator.cli.tests;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
 import static org.junit.Assert.assertEquals;
 
-import com.calculator.cli.Main;
-
 public class CorrectUserInputMainIT {
-	ByteArrayOutputStream outputByteStream;
-	PrintStream testingOutputPrintStream;
-	PrintStream originalOutputStream;
-	
-	@Before
-	public void setUp() {
-		this.outputByteStream = new ByteArrayOutputStream();
-		this.testingOutputPrintStream = new PrintStream(this.outputByteStream);
-		this.originalOutputStream = System.out;
-		
-		System.setOut(this.testingOutputPrintStream);
-	}
-	
-	@After
-	public void tearDown() {
-		System.out.flush();
-		System.setOut(this.originalOutputStream);
-	}
+	public static String PATH_TO_JAR = "../calculator-cli/target/calculator-cli-1.0-SNAPSHOT-jar-with-dependencies.jar";
 	
 	@Test
-	public void calculateCorrectExpression() {
-		Main.main(new String[] {"(1+2)*3"});
+	public void calculateCorrectExpression() throws IOException {
+		ProcessBuilder pb = new ProcessBuilder("java", "-jar", PATH_TO_JAR, "(1+2)*3");
+		Process p = pb.start();
+		InputStreamReader is = new InputStreamReader(p.getInputStream());
 		
-		assertEquals("The expression result is 9.00.", this.outputByteStream.toString());
+		BufferedReader br = new BufferedReader(is);
+		
+		assertEquals("The expression result is 9.00.", br.readLine());
+		
+		p.destroy();
 	}
 }
