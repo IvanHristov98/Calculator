da5e60744332e7766536d7eba012df26ead0867b
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-30 17:38:19 +0300

    Add CalculatorDecorator class.

---
+++ calculator-core/src/main/java/com/calculator/core/CalculatorDecorator.java
@@ -0,0 +1,9 @@
+package com.calculator.core;
+
+public abstract class CalculatorDecorator implements iCalculator {
+	protected iCalculator calculator;
+	
+	public CalculatorDecorator(iCalculator calculator) {
+		this.calculator = calculator;
+	}
+}
