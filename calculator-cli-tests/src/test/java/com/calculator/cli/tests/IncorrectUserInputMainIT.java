ce794e1116cf0a78663c5a332f2ec432b9f67af7
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-30 16:06:18 +0300

    Modify IncorrectUserInputMainIT to use the jar of calculator-cli.

--- calculator-cli-tests/src/test/java/com/calculator/cli/tests/IncorrectUserInputMainIT.java
+++ calculator-cli-tests/src/test/java/com/calculator/cli/tests/IncorrectUserInputMainIT.java
@@ -2,102 +2,77 @@ package com.calculator.cli.tests;
 
 import static org.junit.Assert.assertEquals;
 
-import java.io.ByteArrayOutputStream;
-import java.io.PrintStream;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 
-import org.junit.After;
-import org.junit.Before;
 import org.junit.Test;
 
-import com.calculator.cli.Main;
-
 public class IncorrectUserInputMainIT {
-	ByteArrayOutputStream errorByteStream;
-	PrintStream testingErrorPrintStream;
-	PrintStream originalErrorPrintStream;
+	public static String PATH_TO_JAR = "../calculator-cli/target/calculator-cli-1.0-SNAPSHOT-jar-with-dependencies.jar";
 	
-	@Before
-	public void setUp() {
-		this.errorByteStream = new ByteArrayOutputStream();
-		this.testingErrorPrintStream = new PrintStream(this.errorByteStream);
-		this.originalErrorPrintStream = System.err;
-		
-		System.setErr(this.testingErrorPrintStream);
-	}
-	
-	@After
-	public void tearDown() {
-		System.err.flush();
-		System.setErr(this.originalErrorPrintStream);
+	@Test
+	public void invalidNumberOfArguments_main() throws IOException {
+		testCalculatorErrorStream("Invalid number of arguments. Only one argument is expected.", "1", "2");
 	}
 	
 	@Test
-	public void invalidNumberOfArguments_main() {
-		Main.main(new String[] {"1", "2"});
-		
-		assertEquals(
-				"Invalid number of arguments. Only one argument is expected.", 
-				this.errorByteStream.toString()
-				);
+	public void operatorMisplacement_main() throws IOException {
+		testCalculatorErrorStream("Expression error. Operator misplacement error encountered.", "1+*2");
 	}
 	
 	@Test
-	public void operatorMisplacement_main() {
-		Main.main(new String[] {"1+*2"});
-		
-		assertEquals(
-				"Expression error. Operator misplacement error encountered.",
-				this.errorByteStream.toString()
-				);
+	public void bracketRelatedError_main() throws IOException {
+		testCalculatorErrorStream("Expression error. Brackets error encountered.", "(1))");
 	}
 	
 	@Test
-	public void bracketRelatedError_main() {
-		Main.main(new String[] {"(1))"});
-		
-		assertEquals(
-				"Expression error. Brackets error encountered.",
-				this.errorByteStream.toString()
-				);
+	public void divisionByZero_main() throws IOException {
+		testCalculatorErrorStream("Expression error. Division by zero encountered.", "1/0");
 	}
 	
 	@Test
-	public void divisionByZero_main() {
-		Main.main(new String[] {"1/0"});
-		
-		assertEquals(
-				"Expression error. Division by zero encountered.",
-				this.errorByteStream.toString()
-				);
+	public void emptyExpression_main() throws IOException {
+		testCalculatorErrorStream("Expression error. Empty expression encountered.", " ");
 	}
 	
 	@Test
-	public void emptyExpression_main() {
-		Main.main(new String [] {""});
-		
-		assertEquals(
-				"Expression error. Empty expression encountered.",
-				this.errorByteStream.toString()
-				);
+	public void invalidOperator_main() throws IOException {
+		testCalculatorErrorStream("Expression error. Invalid operators encountered. Valid operator symbols are +, -, *, / and ^.", "1A5");
 	}
 	
 	@Test
-	public void invalidOperator_main() {
-		Main.main(new String [] {"1A5"});
-		
-		assertEquals(
-				"Expression error. Invalid operators encountered. Valid operator symbols are +, -, *, / and ^.",
-				this.errorByteStream.toString()
-				);
+	public void numberMisplacement_main() throws IOException {
+		testCalculatorErrorStream("Expression error. Number misplacement encountered. Numbers should be separated by arithmetic operators.", "1 2");
 	}
 	
-	@Test
-	public void numberMisplacement_main() {
-		Main.main(new String [] {"1 2"});
+	public void testCalculatorErrorStream(String expectedResult, String... commandArguments) throws IOException {
+		String[] defaultJarCallingPrefixArguments = new String[] {"java", "-jar", PATH_TO_JAR};
+		commandArguments = mergeStringArrays(defaultJarCallingPrefixArguments, commandArguments);
 		
-		assertEquals(
-				"Expression error. Number misplacement encountered. Numbers should be separated by arithmetic operators.",
-				this.errorByteStream.toString()
-				);
+		ProcessBuilder processBuilder = new ProcessBuilder(commandArguments);
+		Process process = processBuilder.start();
+		InputStreamReader streamReader = new InputStreamReader(process.getErrorStream());
+		BufferedReader bufferedReader = new BufferedReader(streamReader);
+		
+		assertEquals(expectedResult, bufferedReader.readLine());
+		
+		process.destroy();
 	}
+	
+	public String[] mergeStringArrays(String[] firstArray, String[] secondArray) {
+		String[] resultArray = (String[]) Array.newInstance(String.class, firstArray.length + secondArray.length);
+		
+		for (int i = 0; i < firstArray.length; ++i) {
+			resultArray[i] = firstArray[i];
+		}
+		
+		for (int i = firstArray.length; i < firstArray.length + secondArray.length; ++i) {
+			resultArray[i] = secondArray[i - firstArray.length];
+		}
+		
+		return resultArray;
+	}
+	
 }
