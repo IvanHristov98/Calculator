0f204282aeec4968a0e7af53c5c5242ee44cd8a6
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-07-31 15:19:20 +0300

    Modify rethrown exceptions to use cause.
    Via the overloaded constructor.

--- calculator-cli/src/main/java/com/calculator/cli/coreWrapper/ExceptionWrappingCalculator.java
+++ calculator-cli/src/main/java/com/calculator/cli/coreWrapper/ExceptionWrappingCalculator.java
@@ -12,19 +12,19 @@ public class ExceptionWrappingCalculator extends CalculatorDecorator {
 		try {
 			return calculator.calculate(expression);
 		} catch (BracketsException exception) {
-			throw new BracketsException("Expression error. Brackets error encountered.");
+			throw new BracketsException("Expression error. Brackets error encountered.", exception);
 		} catch (OperatorMisplacementException exception) {
-			throw new OperatorMisplacementException("Expression error. Operator misplacement error encountered.");
+			throw new OperatorMisplacementException("Expression error. Operator misplacement error encountered.", exception);
 		} catch (InvalidOperatorException exception) {
 			throw new InvalidOperatorException(
-					"Expression error. Invalid operators encountered. Valid operator symbols are +, -, *, / and ^.");
+					"Expression error. Invalid operators encountered. Valid operator symbols are +, -, *, / and ^.", exception);
 		} catch (DivisionByZeroException exception) {
-			throw new DivisionByZeroException("Expression error. Division by zero encountered.");
+			throw new DivisionByZeroException("Expression error. Division by zero encountered.", exception);
 		} catch (EmptyExpressionException exception) {
-			throw new EmptyExpressionException("Expression error. Empty expression encountered.");
+			throw new EmptyExpressionException("Expression error. Empty expression encountered.", exception);
 		} catch (NumberMisplacementException exception) {
 			throw new NumberMisplacementException(
-					"Expression error. Number misplacement encountered. Numbers should be separated by arithmetic operators.");
+					"Expression error. Number misplacement encountered. Numbers should be separated by arithmetic operators.", exception);
 		}
 	}
 }
