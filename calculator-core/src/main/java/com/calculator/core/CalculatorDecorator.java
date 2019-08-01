fcd2481b46cf193b4cdffdf38af51838a039b78d
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-01 08:49:20 +0300

    Remove CalculatorDecorator because of redundancy.

--- calculator-core/src/main/java/com/calculator/core/CalculatorDecorator.java
+++
@@ -1,9 +0,0 @@
-package com.calculator.core;
-
-public abstract class CalculatorDecorator implements ICalculator {
-	protected ICalculator calculator;
-
-	public CalculatorDecorator(ICalculator calculator) {
-		this.calculator = calculator;
-	}
-}
