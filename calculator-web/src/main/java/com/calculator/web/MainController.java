8939b57595a4c45bb1a38e8f33da173d324534f4
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-05 14:35:01 +0300

    Modify MainController to deal with calculate GET request.

--- calculator-web/src/main/java/com/calculator/web/MainController.java
+++ calculator-web/src/main/java/com/calculator/web/MainController.java
@@ -1,17 +1,44 @@
 package com.calculator.web;
 
+import com.calculator.core.CalculatorFactory;
+import com.calculator.core.Expression;
+import com.calculator.web.wrapper.*;
+
 import java.io.IOException;
 import java.io.PrintWriter;
 
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;;
+import javax.servlet.http.HttpServletResponse;
 
 @SuppressWarnings("serial")
 public class MainController extends HttpServlet {
-	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		PrintWriter pw = resp.getWriter();
-		pw.println("Hello world!");
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		response.setContentType("text/html");
+		
+		PrintWriter printWriter = response.getWriter();
+		
+		try {
+			executeCalculation(request.getParameter("calculate"));
+		} catch (Exception e) {
+			printWriter.print(e.getMessage());
+		}
+	}
+	
+	private String executeCalculation(String expressionContent) throws Exception {
+		if (expressionContent == null) {
+			return "";
+		}
+		
+		return calculate(expressionContent).toString();
+	}
+	
+	private Double calculate(String expessionContent) throws Exception {
+		CalculatorFactory factory = new CalculatorFactory();
+		ICalculator calc = new CalculatorAdapter(factory.makeCalculator());
+		Expression expression = new Expression(expessionContent);
+		
+		return calc.calculate(expression);
 	}
 }
