0862a95ea74001c2239c47b1f83618ecb0414005
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-05 12:45:32 +0300

    Modify MainController to extend HttpServlet.

--- calculator-web/src/main/java/com/calculator/web/MainController.java
+++ calculator-web/src/main/java/com/calculator/web/MainController.java
@@ -1,5 +1,17 @@
 package com.calculator.web;
 
-public class MainController {
+import java.io.IOException;
+import java.io.PrintWriter;
 
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;;
+
+@SuppressWarnings("serial")
+public class MainController extends HttpServlet {
+	protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		PrintWriter pw = resp.getWriter();
+		pw.println("Hello world!");
+	}
 }
