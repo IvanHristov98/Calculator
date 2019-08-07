251a3269753d7a4904d82d5a832ed551e68dbba7
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-07 17:37:33 +0300

    Add calculator-web jar file to shrinkwrap building.

--- calculator-web-tests/src/test/java/com/calculator/web/tests/GreeterTest.java
+++ calculator-web-tests/src/test/java/com/calculator/web/tests/GreeterTest.java
@@ -1,11 +1,16 @@
 package com.calculator.web.tests;
 
+import java.net.URL;
+
 import javax.inject.Inject;
  
 import org.jboss.arquillian.container.test.api.Deployment;
 import org.jboss.arquillian.junit.Arquillian;
+import org.jboss.arquillian.test.api.ArquillianResource;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
+import org.jboss.shrinkwrap.api.exporter.StreamExporter;
+import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
 import org.junit.Assert;
 import org.junit.Test;
@@ -22,9 +27,11 @@ public class GreeterTest {
  
     @Deployment
     public static WebArchive createDeployment() {
+    	JavaArchive jar = ShrinkWrap.create(JavaArchive.class).addAsDirectories("target/lib/calculator-web-1.0-SNAPSHOT-classes.jar");
         WebArchive war =  ShrinkWrap.create(WebArchive.class)
             .addClass(Greeter.class)
             .addClass(PhraseBuilder.class)
+            .addAsLibraries(jar)
             .addAsManifestResource("arquillian.xml")
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
             .setWebXML("WEB-INF/web.xml");
