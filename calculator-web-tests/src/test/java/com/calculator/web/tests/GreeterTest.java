3711482b04daa5f4f3117c76d4f58f3285a52e51
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-07 17:00:49 +0300

    Add calculator-web as a dependency.

--- calculator-web-tests/src/test/java/com/calculator/web/tests/GreeterTest.java
+++ calculator-web-tests/src/test/java/com/calculator/web/tests/GreeterTest.java
@@ -1,7 +1,5 @@
 package com.calculator.web.tests;
- 
-import java.io.File;
- 
+
 import javax.inject.Inject;
  
 import org.jboss.arquillian.container.test.api.Deployment;
@@ -9,7 +7,6 @@ import org.jboss.arquillian.junit.Arquillian;
 import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.asset.EmptyAsset;
 import org.jboss.shrinkwrap.api.spec.WebArchive;
-import org.jboss.shrinkwrap.resolver.impl.maven.MavenDependencyResolverSettings;;
 import org.junit.Assert;
 import org.junit.Test;
 import org.junit.runner.RunWith;
@@ -19,21 +16,18 @@ public class GreeterTest {
  
     @Inject
     Greeter greeter;
+    
+    //@ArquillianResource
+    //private URL base;
  
     @Deployment
     public static WebArchive createDeployment() {
-       /* File[] lib = MavenConverter.
-                .resolve("org.jboss.weld.servlet:weld-servlet:1.1.9.Final")
-                .withTransitivity().as(File.class);*/
- 
         WebArchive war =  ShrinkWrap.create(WebArchive.class)
             .addClass(Greeter.class)
             .addClass(PhraseBuilder.class)
             .addAsManifestResource("arquillian.xml")
-           // .addAsLibraries(lib)
             .addAsWebInfResource(EmptyAsset.INSTANCE, "beans.xml")
-            .setWebXML("WEB-INF/web.xml")
-            ;
+            .setWebXML("WEB-INF/web.xml");
  
         System.out.println(war.toString(true));
  
