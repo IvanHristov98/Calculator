cf7cd4f6fced1c67901befb1fa965bf91f6a6288
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-12 18:08:07 +0300

    Modify CalculationServlet to return calculation result in json.

--- calculator-web/src/main/java/com/calculator/web/CalculationServlet.java
+++ calculator-web/src/main/java/com/calculator/web/CalculationServlet.java
@@ -4,6 +4,7 @@ import com.calculator.core.CalculatorFactory;
 import com.calculator.core.Expression;
 import com.calculator.web.wrapper.*;
 import com.calculator.web.wrapper.exception.WebCalculatorException;
+import com.google.gson.Gson;
 
 import java.io.*;
 import java.net.MalformedURLException;
@@ -16,16 +17,24 @@ import javax.servlet.http.*;
 public class CalculationServlet extends HttpServlet {
 	
 	public static final int NUMBER_OF_URL_PATH_ITEMS = 5;
+	// Gson objects are stateless thus is it safe
+	private Gson gson = new Gson();
+	
 	
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		response.setContentType("text/json");
+		response.setContentType("application/json");
 		
 		PrintWriter printWriter = response.getWriter();
 		
 		try {
-			printWriter.print(getCaclulationResult(request));
+			CalculationResult result = new CalculationResult(getCaclulationResult(request));
+			
+			printWriter.print(gson.toJson(result));
 		} catch (Exception e) {
 			printWriter.print(e.getMessage());
+		}
+		finally {
+			printWriter.flush();
 		}
 	}
 	
@@ -41,7 +50,11 @@ public class CalculationServlet extends HttpServlet {
 	
 	private String getExpressionContentFromRequestURL(URL requestUrl) {
 		String[] requestUrlPathElements = requestUrl.getPath().split("/", NUMBER_OF_URL_PATH_ITEMS);
-		return requestUrlPathElements[5-1];
+		return requestUrlPathElements[getExpressionIndexInUrlPathArray()];
+	}
+	
+	private int getExpressionIndexInUrlPathArray() {
+		return NUMBER_OF_URL_PATH_ITEMS-1;
 	}
 	
 	private Double calculate(String expessionContent) throws WebCalculatorException  {
