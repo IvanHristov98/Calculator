c37f5c4e1c3fca1c29f90d2d0b20d7eec4d4e787
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-14 10:28:15 +0300

    Add calculation functionality to CalculateResource.

--- calculator-web/src/main/java/com/calculator/web/resources/CalculateResource.java
+++ calculator-web/src/main/java/com/calculator/web/resources/CalculateResource.java
@@ -2,9 +2,17 @@ package com.calculator.web.resources;
 
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
+import javax.ws.rs.core.Response;
 
+import com.calculator.core.CalculatorFactory;
+import com.calculator.core.Expression;
 import com.calculator.web.CalculationResult;
+import com.calculator.web.resourceRepresentations.HttpError;
+import com.calculator.web.wrapper.CalculatorAdapter;
+import com.calculator.web.wrapper.ICalculator;
+import com.calculator.web.wrapper.exception.WebCalculatorException;
 
 import javax.ws.rs.GET;
 
@@ -13,9 +21,23 @@ public class CalculateResource {
 	
 	@GET
 	@Produces(MediaType.APPLICATION_JSON)
-	public CalculationResult getCalculationResult() {
-		CalculationResult result = new CalculationResult("5");
+	public Response getCalculationResult(@QueryParam("expression") String expressionContent) {
+		try {
+			Double calculationResult = calculate(expressionContent);
+			CalculationResult calculationResultAsPojo = new CalculationResult(calculationResult.toString());
+			
+			return Response.status(Response.Status.OK).entity(calculationResultAsPojo).build();
+		} catch (WebCalculatorException exception) {
+			HttpError httpError = new HttpError(Response.Status.BAD_REQUEST.toString(), exception.getMessage());
+			return Response.status(Response.Status.BAD_REQUEST).entity(httpError).build();
+		}
+	}
+	
+	private Double calculate(String expessionContent) throws WebCalculatorException  {
+		CalculatorFactory factory = new CalculatorFactory();
+		ICalculator calc = new CalculatorAdapter(factory.makeCalculator());
+		Expression expression = new Expression(expessionContent);
 		
-		return result;
+		return calc.calculate(expression);
 	}
 }
