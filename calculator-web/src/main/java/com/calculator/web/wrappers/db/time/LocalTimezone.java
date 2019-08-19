351fd41da9f5a87b2da87fd325e6349db781242b
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-19 13:34:42 +0300

    Add a timezone handling db subpackage.

---
+++ calculator-web/src/main/java/com/calculator/web/wrappers/db/time/LocalTimezone.java
@@ -0,0 +1,10 @@
+package com.calculator.web.wrappers.db.time;
+
+import java.util.Calendar;
+import java.util.TimeZone;
+
+public class LocalTimezone {
+	
+	public static final Calendar TIME_ZONE_UTC = Calendar.getInstance(TimeZone.getTimeZone("UTC"));
+	
+}
