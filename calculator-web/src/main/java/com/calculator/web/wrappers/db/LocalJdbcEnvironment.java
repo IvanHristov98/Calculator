6cbfe2d1565c4a81791e3193557d42c8b21f67d0
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-08-21 15:18:22 +0300

    Switch to using environment variables to acces db information.

--- calculator-web/src/main/java/com/calculator/web/wrappers/db/LocalJdbcEnvironment.java
+++ calculator-web/src/main/java/com/calculator/web/wrappers/db/LocalJdbcEnvironment.java
@@ -3,13 +3,13 @@ package com.calculator.web.wrappers.db;
 public class LocalJdbcEnvironment {
 	
 	public static final String JDBC = "jdbc";
-	public static final String JDBC_SUBPROTOCOL = "JDBC_SUBPROTOCOL";
-	public static final String JDBC_HOST = "JDBC_HOST";
-	public static final String JDBC_PORT = "JDBC_PORT";
-	public static final String JDBC_DATABASE = "JDBC_DATABASE";
+	public static final String JDBC_SUBPROTOCOL = "CALCULATOR_JDBC_SUBPROTOCOL";
+	public static final String JDBC_HOST = "CALCULATOR_JDBC_HOST";
+	public static final String JDBC_PORT = "CALCULATOR_JDBC_PORT";
+	public static final String JDBC_DATABASE = "CALCULATOR_JDBC_DATABASE";
 	
-	public static final String JDBC_USER = "JDBC_USER";
-	public static final String JDBC_PASSWORD = "JDBC_PASSWORD";
+	public static final String JDBC_USER = "CALCULATOR_JDBC_USER";
+	public static final String JDBC_PASSWORD = "CALCULATOR_JDBC_PASSWORD";
 	
 	public String getDatabaseUrl() {
 		return 	JDBC + ":" +
@@ -23,30 +23,30 @@ public class LocalJdbcEnvironment {
 	}
 	
 	public String getSubprotocol() {
-		return "postgresql";
+		return getSystemEnvironment(JDBC_SUBPROTOCOL);
 	}
 	
 	public String getHost() {
-		return "localhost";
+		return getSystemEnvironment(JDBC_HOST);
 	}
 	
 	public String getPort() {
-		return "5432";
+		return getSystemEnvironment(JDBC_PORT);
 	}
 	
 	public String getDatabase() {
-		return "calculator_db";
+		return getSystemEnvironment(JDBC_DATABASE);
 	}
 	
 	public String getUser() {
-		return "calculator";
+		return getSystemEnvironment(JDBC_USER);
 	}
 	
 	public String getPassword() {
-		return "calculator";
+		return getSystemEnvironment(JDBC_PASSWORD);
 	}
 	
-	private String getSystemEnvironment(String variable) {
-		return System.getenv(variable);
+	private String getSystemEnvironment(String variableName) {
+		return System.getenv(variableName);
 	}
 }
