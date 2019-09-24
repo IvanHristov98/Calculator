87421e72150b403c2f4b9fa874015de2ec5533d7
Ivan Hristov <35896427+IvanHristov98@users.noreply.github.com>
2019-09-24 11:10:57 +0300

    Add Calculator UI.

--- calculator-web-ui/src/main/webapp/controller/Calculator.controller.js
+++ calculator-web-ui/src/main/webapp/controller/Calculator.controller.js
@@ -1,30 +1,111 @@
 sap.ui.define([
     "sap/ui/core/mvc/Controller",
-], function (Controller) {
+    "sap/ui/model/json/JSONModel"
+], function (Controller, JSONModel, Calculator) {
    "use strict";
 
    return Controller.extend("com.calculator.webUi.controller.Calculator", {
-
-      onTokenPress : function (token) {
-         let expressionBar = this.getExpressionBar();
-         let currentExpression = expressionBar.getValue();
-         expressionBar.setValue(currentExpression + token);
+      calculateResource : "https://calculator.cfapps.sap.hana.ondemand.com/api/v1/calculate",
+      calculationResultsResource : "https://calculator.cfapps.sap.hana.ondemand.com/api/v1/calculationResults",
+      intervalBetweenCompletionChecks: 1000,
+      onTokenPress : function (sToken) {
+         let oExpressionBar = this.getExpressionBar();
+         let sCurrentExpression = oExpressionBar.getValue();
+         oExpressionBar.setValue(sCurrentExpression + sToken);
       },
       onBackspaceButtonPress : function () {
-         let expressionBar = this.getExpressionBar();
-         let currentExpression = expressionBar.getValue();
-         expressionBar.setValue(currentExpression.substring(0, currentExpression.length - 1));
+         let oExpressionBar = this.getExpressionBar();
+         let sCurrentExpression = oExpressionBar.getValue();
+         oExpressionBar.setValue(sCurrentExpression.substring(0, sCurrentExpression.length - 1));
       },
       onDeleteAllButtonPress : function () {
-         let expressionBar = this.getExpressionBar();
-         expressionBar.setValue("");
+         let oExpressionBar = this.getExpressionBar();
+         oExpressionBar.setValue("");
+      },
+      onCalculateButtonPress : function () {
+         this.uploadExpression();
       },
       getExpressionBar : function () {
-         let view = this.getCurrentView();
-         return view.byId("expressionBar");
+         let oView = this.getCurrentView();
+         return oView.byId("expressionBar");
       },
       getCurrentView : function () {
          return this.getView();
+      },
+      uploadExpression : function () {
+         let oCalculator = this;
+         let onSuccessfulExpressionRecording = function () {
+            oCalculator.startResultRetrievingJob(this.readyState, this.responseText, oCalculator);
+         };
+
+         let oExpressionBar = this.getExpressionBar();
+
+         let oHttpRequestDetails = {
+            resource : this.calculateResource,
+            method: "POST",
+            readyStateListener : onSuccessfulExpressionRecording,
+            data : oExpressionBar.getValue()
+         };
+         let oXhr = new XMLHttpRequest();
+
+         this.runHttpRequest(oXhr, oHttpRequestDetails);
+      },
+      getCompletedCalculationResult : function (oCalculationResultId, iCalculationResultGettingInterval, oCalculator) {
+         let checkIfCalculationIsCompleted = function () {
+            oCalculator.checkIfCalculationIsCompleted(this.readyState, this.responseText, iCalculationResultGettingInterval);
+         };
+
+         let oHttpRequestDetails = {
+            resource : oCalculator.calculationResultsResource + "/" + oCalculationResultId,
+            method: "GET",
+            readyStateListener : checkIfCalculationIsCompleted,
+            data : null
+         };
+         let oXhr = new XMLHttpRequest();
+
+         oCalculator.runHttpRequest(oXhr, oHttpRequestDetails);
+      },
+      runHttpRequest : function (oXhr, oHttpRequestDetails) {
+         oXhr.open(oHttpRequestDetails.method, oHttpRequestDetails.resource);
+         oXhr.setRequestHeader("Content-Type", "application/json");
+         oXhr.setRequestHeader("cache-control", "no-cache");
+         oXhr.addEventListener("readystatechange", oHttpRequestDetails.readyStateListener);
+         
+         oXhr.send(oHttpRequestDetails.data);
+      },
+      startResultRetrievingJob : function (iReadyState, sResponseText, oCalculator) {
+         if (iReadyState === 4) {
+            let iCalculationResultId = sResponseText;
+            
+            let iCalculationResultGettingInterval = setInterval(function () { 
+               oCalculator.getCompletedCalculationResult(iCalculationResultId, iCalculationResultGettingInterval, oCalculator);
+            }, oCalculator.intervalBetweenCompletionChecks);
+         }
+      },
+      checkIfCalculationIsCompleted : function (iReadyState, sResponseText, iCalculationResultGettingInterval) {
+         if (iReadyState === 4) {
+            let oCalculationResult = JSON.parse(sResponseText);
+
+            if (oCalculationResult.status != 2) {
+               return;
+            }
+
+            clearInterval(iCalculationResultGettingInterval);
+
+            let oExpressionBar = this.getExpressionBar();
+
+            if (this.isExpressionCorrect(oCalculationResult)) {                  
+               oExpressionBar.setValue(oCalculationResult.evaluation);
+            } else if (this.isExpressionIncorrect(oCalculationResult)) {
+               oExpressionBar.setValue(oCalculationResult.message);
+            }
+         }
+      },
+      isExpressionCorrect : function (oCalculationResult) {
+         return oCalculationResult.evaluation !== null && oCalculationResult.message === null;
+      },
+      isExpressionIncorrect : function (oCalculationResult) {
+         return oCalculationResult.evaluation === null && oCalculationResult.message !== null;
       }
    });
 });
--- calculator-web-ui/src/main/webapp/test/unit/controller/Calculator.controller.js
+++ calculator-web-ui/src/main/webapp/test/unit/controller/Calculator.controller.js
@@ -9,14 +9,20 @@ sap.ui.define([
 ], function (ManagedObject, Controller, Input, CalculatorController, JSONModel, sinon, sinonQunit) {
     "use strict";
 
+    let sandbox;
+
     QUnit.module("Calculator controller test", {
         beforeEach : function () {
+            sandbox = sinon.sandbox.create();
+
             this._oCalculatorController = new CalculatorController();
 
             this._oViewStub = new ManagedObject();
             this._oGetViewStub = sinon.stub(Controller.prototype, "getView").returns(this._oViewStub);
         },
         afterEach : function () {
+            sandbox.restore();
+
             this._oGetViewStub.restore();
         }
     });
@@ -26,7 +32,7 @@ sap.ui.define([
         const justAToken = "1";
 
         let oFakeExpressionBar = getFakeExpressionBar(expression);
-        this._oViewStub.byId = sinon.stub().returns(oFakeExpressionBar);
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
         
         this._oCalculatorController.onTokenPress(justAToken);
         assert.ok(oFakeExpressionBar.setValue.called);
@@ -37,7 +43,7 @@ sap.ui.define([
         const expressionAfterBackspacing = "1";
 
         let oFakeExpressionBar = getFakeExpressionBar(expressionBeforeBackspacing);
-        this._oViewStub.byId = sinon.stub().returns(oFakeExpressionBar);
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
 
         this._oCalculatorController.onBackspaceButtonPress();
         assert.ok(oFakeExpressionBar.setValue.calledWith(expressionAfterBackspacing));
@@ -48,17 +54,164 @@ sap.ui.define([
         const emptyExpression = "";
 
         let oFakeExpressionBar = getFakeExpressionBar(expressionBeforeDeletion);
-        this._oViewStub.byId = sinon.stub().returns(oFakeExpressionBar);
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
 
         this._oCalculatorController.onDeleteAllButtonPress();
         assert.ok(oFakeExpressionBar.setValue.calledWith(emptyExpression));
     });
 
+    QUnit.test("Verify XmlHttpRequest sending", function (assert) {
+        let httpRequestDetails = getFakeHttpRequestDetails();
+        let fakeXhr = getFakeXhr();
+        this._oCalculatorController.runHttpRequest(fakeXhr, httpRequestDetails);
+
+        assert.ok(fakeXhr.send.calledWith(httpRequestDetails.data));
+    });
+
+    QUnit.test("Verify XmlHttpRequest opening", function (assert) {
+        let httpRequestDetails = getFakeHttpRequestDetails();
+        let fakeXhr = getFakeXhr();
+        this._oCalculatorController.runHttpRequest(fakeXhr, httpRequestDetails);
+
+        assert.ok(fakeXhr.open.calledWith(httpRequestDetails.method, httpRequestDetails.resource));
+    });
+
+    QUnit.test("Verify XmlHttpRequest setting of ready state listener", function (assert) {
+        let httpRequestDetails = getFakeHttpRequestDetails();
+        let fakeXhr = getFakeXhr();
+        this._oCalculatorController.runHttpRequest(fakeXhr, httpRequestDetails);
+
+        assert.ok(fakeXhr.addEventListener.calledWith("readystatechange", httpRequestDetails.readyStateListener));
+    });
+
+    QUnit.test("Verify behaviour with correct expression", function (assert) {
+        let fakeCalculationResult = {
+            evaluation : 1.0,
+            message : null
+        };
+
+        assert.ok(this._oCalculatorController.isExpressionCorrect(fakeCalculationResult));
+        assert.notOk(this._oCalculatorController.isExpressionIncorrect(fakeCalculationResult));
+    });
+
+    QUnit.test("Verify behaviour with incorrect expression", function (assert) {
+        let fakeCalculationResult = {
+            evaluation : null,
+            message : "Error."
+        };
+
+        assert.notOk(this._oCalculatorController.isExpressionCorrect(fakeCalculationResult));
+        assert.ok(this._oCalculatorController.isExpressionIncorrect(fakeCalculationResult));
+    });
+
+    QUnit.test("Verify no actions at expression pending status", function (assert) {
+        let iReadyState = 4;
+        let sResponseText = '{"status" : 1}';
+        let oCalculationResultGettingInterval = {};
+
+        let oFakeExpressionBar = getFakeExpressionBar("");
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
+
+        this._oCalculatorController.checkIfCalculationIsCompleted(iReadyState, sResponseText, oCalculationResultGettingInterval);
+
+        assert.notOk(oFakeExpressionBar.setValue.called);
+    });
+
+    QUnit.test("Verify expression bar setting with correct value", function (assert) {
+        let iReadyState = 4;
+        let iExpectedEvaluation = 1.0;
+        let sResponseText = '{"status" : 2, "message" : null, "evaluation" : ' + iExpectedEvaluation + '}';
+        let oCalculationResultGettingInterval = {};
+
+        let oFakeExpressionBar = getFakeExpressionBar("");
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
+        
+        this._oCalculatorController.checkIfCalculationIsCompleted(iReadyState, sResponseText, oCalculationResultGettingInterval);
+
+        assert.ok(oFakeExpressionBar.setValue.calledWith(iExpectedEvaluation));
+    });
+
+    QUnit.test("Verify expression bar setting with incorrect value", function (assert) {
+        let iReadyState = 4;
+        let sExpectedError = "ExpectedError";
+        let sResponseText = '{"status" : 2, "message" : "' + sExpectedError + '", "evaluation" : null}';
+        let oCalculationResultGettingInterval = {};
+        
+        let oFakeExpressionBar = getFakeExpressionBar("");
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
+        
+        this._oCalculatorController.checkIfCalculationIsCompleted(iReadyState, sResponseText, oCalculationResultGettingInterval);
+
+        assert.ok(oFakeExpressionBar.setValue.calledWith(sExpectedError));
+    });
+
+    QUnit.test("Verify the start of retrieving job", function (assert) {
+        const clock = sinon.useFakeTimers();
+        const expectedIntervalBetweenCalls = 1000;
+
+        let iReadyState = 4;
+        let sResponseText = "1";
+
+        const oCalculator = new CalculatorController();
+        oCalculator.getCompletedCalculationResult = sandbox.stub();
+
+        oCalculator.startResultRetrievingJob(iReadyState, sResponseText, oCalculator);
+
+        clock.tick(expectedIntervalBetweenCalls);
+        assert.ok(oCalculator.getCompletedCalculationResult.calledOnce);
+
+        clock.restore();
+    });
+
+    QUnit.test("Verify expression uploading", function (assert) {
+        let oFakeExpressionBar = getFakeExpressionBar("");
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
+
+        this._oCalculatorController.runHttpRequest = sandbox.stub();
+        this._oCalculatorController.uploadExpression();
+
+        assert.ok(this._oCalculatorController.runHttpRequest.calledOnce);
+    });
+
+    QUnit.test("Verify getting of completed calculation results.", function (assert) {
+        let oFakeExpressionBar = getFakeExpressionBar("");
+        this._oViewStub.byId = sandbox.stub().returns(oFakeExpressionBar);
+
+        const oCalculationResultId = "1";
+        const iCalculationResultGettingInterval = 1;
+
+        const oCalculator = {};
+        oCalculator.runHttpRequest = sandbox.stub();
+
+        this._oCalculatorController.getCompletedCalculationResult(oCalculationResultId, iCalculationResultGettingInterval, oCalculator);
+
+        assert.ok(oCalculator.runHttpRequest.calledOnce);
+    });
+
     function getFakeExpressionBar(sExpressionContent) {
         let oFakeExpressionBar = new Input();
-        oFakeExpressionBar.getValue = sinon.stub().returns(sExpressionContent);
-        oFakeExpressionBar.setValue = sinon.stub();
+        oFakeExpressionBar.getValue = sandbox.stub().returns(sExpressionContent);
+        oFakeExpressionBar.setValue = sandbox.stub();
 
         return oFakeExpressionBar;
+    }
+
+    function getFakeHttpRequestDetails() {
+        return {
+            resource : "nowhere",
+            method: "GET",
+            readyStateListener : function () {},
+            data : null
+        };
+    }
+
+    function getFakeXhr() {
+        let fakeXhr = {};
+        fakeXhr.open = sandbox.stub();
+        fakeXhr.setRequestHeader = sandbox.stub();
+        fakeXhr.addEventListener = sandbox.stub();
+        fakeXhr.send = sandbox.stub();
+
+        return fakeXhr;
     }
 });
